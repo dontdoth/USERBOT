acb83c115463ed71c58a8e2abe276cfb763a741e
Honeyxslayer <163923981+Honeyxslayer@users.noreply.github.com>
2024-04-20 08:43:44 +0530

    Update start.py

--- X/modules/bot/start.py
+++ X/modules/bot/start.py
@@ -15,7 +15,7 @@ def pmowner(client, callback_query):
     client.answer_callback_query(callback_query.id, text="Message sent")
 
 logoX = [
-    "https://telegra.ph/file/14a5eb1c01732e912ee32.jpg"
+    "https://graph.org/file/2cbc7bf60a108af4a67b2.jpg"
 ]
 
 alive_logo = random.choice(logoX)
@@ -24,13 +24,13 @@ alive_logo = random.choice(logoX)
 async def start(app, message):
     chat_id = message.chat.id
     file_id = alive_logo
-    caption = "нɛℓℓσ, мʏ Μᴀsᴛᴇʀ ❣️\nИιcɛ Ƭσ Μɛɛт 𝗬σʋ 🤗 !!\nI ɢʋɛƨƨ, тнαт ʏσʋ κиσω мɛ, Ʋнн ʏσʋ ∂σи'т, иρ..\nƜɛℓℓ.\n\n𝗔 Pᴏᴡᴇʀғᴜʟ 𝗔ƨƨɪᴛᴀɴᴛ \n\n 𝐏ᴏᴡᴇʀᴇᴅ 𝐁ʏ 🦋 [Ɖιcтαтσя](t.me/SAIF_DICTATOR)\n\nYᴏᴜ Cᴀɴ Cʜᴀᴛ Wɪᴛʜ Mʏ Mᴀsᴛᴇʀ Tʜʀᴏᴜɢʜ Tʜɪs Bᴏᴛ.\nIғ Yᴏᴜ Wᴀɴᴛ Yᴏᴜʀ Oᴡɴ Assɪᴛᴀɴᴛ Yᴏᴜ Cᴀɴ Dᴇᴘʟᴏʏ Fʀᴏᴍ Bᴜᴛᴛᴏɴ Bᴇʟᴏᴡ."
+    caption = "нɛℓℓσ, мʏ Μᴀsᴛᴇʀ ❣️\nИιcɛ Ƭσ Μɛɛт 𝗬σʋ 🤗 !!\nI ɢʋɛƨƨ, тнαт ʏσʋ κиσω мɛ, Ʋнн ʏσʋ ∂σи'т, иρ..\nƜɛℓℓ.\n\n𝗔 Pᴏᴡᴇʀғᴜʟ 𝗔ƨƨɪᴛᴀɴᴛ \n\n 𝐏ᴏᴡᴇʀᴇᴅ 𝐁ʏ 🦋 [Ɖιcтαтσя](t.me/OgHoneyy)\n\nYᴏᴜ Cᴀɴ Cʜᴀᴛ Wɪᴛʜ Mʏ Mᴀsᴛᴇʀ Tʜʀᴏᴜɢʜ Tʜɪs Bᴏᴛ.\nIғ Yᴏᴜ Wᴀɴᴛ Yᴏᴜʀ Oᴡɴ Assɪᴛᴀɴᴛ Yᴏᴜ Cᴀɴ Dᴇᴘʟᴏʏ Fʀᴏᴍ Bᴜᴛᴛᴏɴ Bᴇʟᴏᴡ."
     reply_markup = InlineKeyboardMarkup([
         [
-            InlineKeyboardButton("𝐒𝗎𝗉𝗉𝗈𝗋𝗍", url="https://t.me/DEAD_GRPCHAT"),
-            InlineKeyboardButton("𝐂𝗁𝖺𝗇𝗇𝖾𝗅", url="https://t.me/SAIFALLBOT"),
-            InlineKeyboardButton("𝐎𝗐𝗇𝖾𝗋", url="https://t.me/SAIF_DICTATOR"),
-            InlineKeyboardButton("𝐑𝖾𝗉𝗈", url="https://github.com/SAIFDEAD/USERBOT"),
+            InlineKeyboardButton("𝐒𝗎𝗉𝗉𝗈𝗋𝗍", url="https://t.me/insanesociety"),
+            InlineKeyboardButton("𝐂𝗁𝖺𝗇𝗇𝖾𝗅", url="https://t.me/Honey_networks"),
+            InlineKeyboardButton("𝐎𝗐𝗇𝖾𝗋", url="https://t.me/OgHoneyy"),
+            InlineKeyboardButton("𝐑𝖾𝗉𝗈", url="https://graph.org/file/cbc212ef48ad91ca15f0a.mp4"),
         ],
     ])
 
